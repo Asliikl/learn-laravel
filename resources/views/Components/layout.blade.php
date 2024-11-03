e3f44c6dd1f29e7c12645ffe4c33e273f22c3c6e
Asliikl <asliikiel9@gmail.com>
2024-11-03 17:28:41 +0300

    end of lesson

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -28,7 +28,6 @@
               <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
               <x-nav-link href="/about"  :active="request()->is('about')">About</x-nav-link>
               <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
-
             </div>
           </div>
         </div>
@@ -48,7 +47,7 @@
                 <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">Open user menu</span>
-                  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                  <img class="h-8 w-8 rounded-full" src="https://cdn-icons-png.flaticon.com/512/6134/6134346.png" alt="">
                 </button>
               </div>
             </div>
